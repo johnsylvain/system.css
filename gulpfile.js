09e0a27fe20cb993d5297f45c71ac6e7552cf129
John Sylvain <john.n.sylvain@gmail.com>
2017-10-21 16:39:33 -0500

    added vertial navigation style

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
   return gulp.src('./src/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'))
     .pipe(gulp.dest('./demo'));
 });

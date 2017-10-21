a2435b4dc1243fae661ee85866f585d86e099352
John Sylvain <john.n.sylvain@gmail.com>
2017-10-21 11:00:42 -0500

    added demo and icon font

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var sass = require('gulp-sass');
 gulp.task('sass', function () {
   return gulp.src('./src/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./demo'));
 });
  
 gulp.task('sass:watch', function () {
